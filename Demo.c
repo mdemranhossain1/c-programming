40445ccf803104afa221e4d7951a5a397b8f750d
Emran Hossain <143733800+mdemranhossain1@users.noreply.github.com>
2026-02-08 00:27:24 +0600

    first commit

---
+++ Demo.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	printf("Print My practical c languager");
+	return0;
+}
---
+++ Practice.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+
+{
+    int num1,num2;
+    printf("Enter first integer number : ");
+    scanf("%d",&num1);
+
+
+    printf("Enter second integer number : ");
+    scanf("%d",&num2);
+
+    printf("%d,%d\n",&num1,num2);
+
+
+
+    return 0;
+
+}
