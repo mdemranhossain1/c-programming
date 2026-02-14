fdd795084f5adb6f9e87a9391328d14f9f8d0a32
Emran Hossain <143733800+mdemranhossain1@users.noreply.github.com>
2026-02-14 21:59:08 +0600

    Some changes

---
+++ Untitled1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char lower;
+    printf("Enter Any Lower Letter : ");
+    scanf("%c",lower-32);
+    printf("The uppercasr Letter : %c",lower-32);
+    return 0;
+}
---
+++ Untitled4.c
@@ -0,0 +1,9 @@
+#includestdio.h>
+int main()
+{
+    char lower;
+    printf("Enter Any Lower Letter : ");
+    scanf("%c",lower);
+    printf("The uppercasr Letter : %c",lower-32);
+
+}
